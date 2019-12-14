6b67269b23ae31bf2192e4ec08eb81fc563a2191
Fernando E.I <mferna.92@gmail.com>
2019-12-13 20:31:28 -0600

    remove lines

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,5 @@
 import Controller from '@ember/controller';
-import { action, get, set } from '@ember/object';
-import { tracked } from "@glimmer/tracking";
 
 export default class ApplicationController extends Controller {
-  @tracked componentName = 'dci';
 
-  @action parentFunc(value) {
-     alert('Hello From Parent:-' + value);
-  }
 }
